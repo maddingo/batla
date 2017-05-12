4439efce8a4cba6592dc3670c80e5a52332e9da2
Martin Goldhahn <martin.goldhahn@tieto.com>
2017-05-12 09:34:29 +0200

    Initial Code generated from https://start.spring.io

---
+++ src/main/java/com/tieto/batla/batla/BatlaApplication.java
@@ -0,0 +1,12 @@
+package com.tieto.batla.batla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatlaApplication.class, args);
+	}
+}
